a21787d4f3b7e457bbdd9fe3fe986fa4f2e8feec
Sumit Kumar <ursumit@gmail.com>
2019-09-17 12:57:32 +0530

    Files

--- src/main/java/com/groovy/Examples/Ch_04_List.groovy
+++ src/main/java/com/groovy/Examples/Ch_04_List.groovy
@@ -1,10 +1,11 @@
 package com.groovy.Examples;
 
 
-class Ch_04_List {
+/*class Ch_04_List {
 
-	static void main(args) {
-		  // ---------- LISTS ----------
+	static void main(args) {*/
+
+   // ---------- LISTS ----------
   // Lists hold a list of objects with an index
 
   def primes = [2,3,5,7,11,13];
@@ -53,6 +54,29 @@ class Ch_04_List {
 
   // Pop last item
   println("Last " + primes.pop());
+  
+  
+  
+  // Searching in a list with find, findall and grep
+  def l1 = ['test', 12, 20, true]
+  // check with grep that one element is a Boolean
+  assert [true] == l1.grep(Boolean)
+  
+  // grep for all elements which start with a pattern
+  assert ['Groovy'] == ['test', 'Groovy', 'Java'].grep(~/^G.*/)
+  
+  // grep for if the list contains b and c
+  assert ['b', 'c'] == ['a', 'b', 'c', 'd'].grep(['b', 'c'])
+  
+  // grep for elements which are contained in the range
+  assert [14, 16] == [5, 14, 16, 75, 12].grep(13..17)
+  
+  // grep for elements which are equal to 42.031
+  assert [42.031] == [15, 'Peter', 42.031, 42.032].grep(42.031)
+  
+  // grep for elements which are larger than 40 based on the closure
+  assert [50, 100, 300] == [10, 12, 30, 50, 100, 300].grep({ it > 40 })
 		
-	}
+/*	}
 }
+*/
